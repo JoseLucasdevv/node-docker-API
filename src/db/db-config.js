00cb63c8b46cef4e34821a8a8c8cb736461d2b22
Jose Lucas <joselucas.dev@gmail.com>
2024-05-31 12:03:32 -0300

    feat:add configDB

---
+++ src/db/db-config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.HOST_DB,
+  database: process.env.DATABASE_NAME,
+  user: process.env.USERNAME_DB,
+  password: process.env.PASSWORD_DB,
+  port: 3306,
+  
+  connectionLimit: 100,
+});
+
+module.exports = db;
