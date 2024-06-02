8ec0ed801ee41548b12a8932696846bbccdbf275
Jose Lucas <joselucas.dev@gmail.com>
2024-06-02 16:07:55 -0300

    feat:add query sql

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ db.connect((err) => {
     console.error("Failed to connect to the database:", err);
     process.exit(1); // Exit the process with an error code
   }
+  const sql = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age TINYINT UNSIGNED NOT NULL
+  )
+`;
 
+  db.query(sql, (err) => {
+    if (err) {
+      console.log("deu pau");
+    }
+  });
+  console.log("db connect");
   app.listen(process.env.PORT_APP, () => {
     console.log(`App running on port ${process.env.PORT_APP}`);
   });
