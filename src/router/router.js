582ce829c5bc18a6663b97490e2b44b222dec6f6
Jose Lucas <joselucas.dev@gmail.com>
2024-06-15 14:14:41 -0300

    feat:add router update

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const {
   createNewUser,
   listAllUsers,
+  updateUser,
 } = require("../controllers/userController");
 
 const router = Router();
@@ -9,5 +10,8 @@ const router = Router();
 router.get("/list", listAllUsers);
 
 router.post("/create", createNewUser);
+
+router.put("/update/:id", updateUser);
+
 
 module.exports = router;
